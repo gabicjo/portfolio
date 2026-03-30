bd807a63eb94500f5b3f60e8957ad0685cf2c3cb
gabicjo <gabriebs@proton.me>
2026-03-30 16:12:16 -0300

    fix: change the function that updates the class based on conditions to use toggle

--- source/js/hamburgerMenu.js
+++ source/js/hamburgerMenu.js
@@ -2,9 +2,5 @@ const hamburgerMenu = document.querySelector("#header-menu");
 const nav = document.querySelector(".nav");
 
 hamburgerMenu.addEventListener("click", () => {
-  if (nav.classList.contains("exibir")) {
-    nav.classList.remove("exibir");
-  } else {
-    nav.classList.add("exibir");
-  }
-});
+    nav.classList.toggle("exibir");
+  });
